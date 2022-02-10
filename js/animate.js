7ee7f97b13ad3081b87e3eea7e148c201b060eae
TWOD0G <1026483692@qq.com>
2022-02-10 17:46:38 +0800

    updata

--- js/animate.js
+++ js/animate.js
@@ -1,13 +1,3 @@
-// setInterval("at()",1000)
-
-
-
-// // function coss(){
-// //     var down_box = document.getElementsByClassName('down_box')
-// //     for(var i=0; i < down_box.length; i++){
-// //         down_box[i].style.left = "0"
-// //     }
-// // }
 var i_d = function introduce_down(){
     // introduce_down到浏览器网页文档顶部的距离(offsetTop指元素顶端到父元素顶端之间的距离)
     var introduce_down = document.getElementById('introduce_down').offsetTop 
@@ -21,18 +11,6 @@ var i_d = function introduce_down(){
             document.getElementsByClassName('down_box')[i].style.left = "0%"
         }
     }
-
 }
 window.onload = i_d
 
-window.onscroll = function (){   
-    var hidden_height = document.body.scrollTop
-    if(hidden_height > 260){
-        var nav = document.getElementById('nav').style
-        var navs = document.getElementById('nav')
-        nav.position = "fixed"
-        document.getElementById('introduce_down').style.height = "50vh"
-        document.getElementById('introduce_up').style.height = "50vh"
-    }
-    i_d
-}
